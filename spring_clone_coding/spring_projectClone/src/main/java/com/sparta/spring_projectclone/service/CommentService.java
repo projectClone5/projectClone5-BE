15aa0302f9148eec16f80604889c8ef09d4abfee
lky8967 <lky8967@gmail.com>
2022-06-18 19:02:11 +0900

    댓글 수정 , 삭제 , 작성

--- spring_clone_coding/spring_projectClone/src/main/java/com/sparta/spring_projectclone/service/CommentService.java
+++ spring_clone_coding/spring_projectClone/src/main/java/com/sparta/spring_projectclone/service/CommentService.java
@@ -3,6 +3,7 @@ package com.sparta.spring_projectclone.service;
 import com.sparta.spring_projectclone.dto.requestDto.CommentRequestDto;
 import com.sparta.spring_projectclone.dto.responseDto.CommentResponseDto;
 import com.sparta.spring_projectclone.model.Comment;
+import com.sparta.spring_projectclone.model.Post;
 import com.sparta.spring_projectclone.repository.CommentRepository;
 import com.sparta.spring_projectclone.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,15 +21,36 @@ public class CommentService {
 
     // 댓글 작성
     @Transactional
-    public CommentResponseDto commentWrite( CommentRequestDto commentRequestDto) {
+    public CommentResponseDto commentWrite(Long postId, CommentRequestDto commentRequestDto) {
 
-//        Post post = postRepository.findById(postId).orElseThrow(
-//                () -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
-        Comment comment = Comment.commentCreateDto(commentRequestDto);
-        commentRepository.save(comment);
+        Post post = postRepository.findById(postId).orElseThrow(
+                () -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
+        Comment comment = new Comment(commentRequestDto , post);
         CommentResponseDto commentResponseDto = new CommentResponseDto(comment);
+        commentRepository.save(comment);
         return commentResponseDto;
 
 
+//        Comment comment = Comment.commentCreateDto(commentRequestDto);
+//        commentRepository.save(comment , post);
+//        CommentResponseDto commentResponseDto = new CommentResponseDto(comment);
+//        return commentResponseDto;
+    }
+
+    //댓글 수정
+    @Transactional
+    public CommentResponseDto commentUpdated(Long commentId, CommentRequestDto commentRequestDto){
+        Comment comment = commentRepository.findById(commentId).orElseThrow(
+                () -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
+        comment.update(commentRequestDto);
+        CommentResponseDto CommentResponseDto = new CommentResponseDto(comment);
+        return CommentResponseDto;
+    }
+
+    // 댓글 삭제
+    public void commentDelete(Long commentId) {
+        Comment comment = commentRepository.findById(commentId).orElseThrow(
+                () -> new IllegalArgumentException("게시글이 존재하지 않습니다."));
+        comment.delete(comment);
     }
 }
