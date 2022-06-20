b01c1eaff3dfc499c9b4124fcffbc64029c9e940
lky8967 <lky8967@gmail.com>
2022-06-20 21:06:47 +0900

    s3 수정 완료 테스트 하기전

--- spring_clone_coding/spring_projectClone/src/main/java/com/sparta/spring_projectclone/service/S3Uploader.java
+++ spring_clone_coding/spring_projectClone/src/main/java/com/sparta/spring_projectclone/service/S3Uploader.java
@@ -4,7 +4,8 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
