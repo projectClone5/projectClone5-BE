fd7be7cf9dec32d7b70d9f7396bea035ef024c6d
lky8967 <lky8967@gmail.com>
2022-06-21 17:23:11 +0900

    comment Dto 주석문 제거

--- spring_clone_coding/spring_projectClone/src/main/java/com/sparta/spring_projectclone/dto/responseDto/CommentResponseDto.java
+++ spring_clone_coding/spring_projectClone/src/main/java/com/sparta/spring_projectclone/dto/responseDto/CommentResponseDto.java
@@ -10,14 +10,12 @@ public class CommentResponseDto {
     private Long id;
     private String comment;
     private int reviewPoint;
-//    private String image;
 
     @Builder
     public CommentResponseDto(Comment comment) {
         this.id = comment.getId();
         this.comment = comment.getComment();
         this.reviewPoint = comment.getReviewPoint();
-//        this.image = comment.getImage();
 
     }
 }
