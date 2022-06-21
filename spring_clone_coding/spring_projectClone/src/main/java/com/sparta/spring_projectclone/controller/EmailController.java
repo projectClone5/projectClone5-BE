41869f9960f7122d3e906293bf9f5945f30ab30f
hyeongjun-Ham <so23go@naver.com>
2022-06-21 23:23:27 +0900

    - 이메일 인증 기능 구현

---
+++ spring_clone_coding/spring_projectClone/src/main/java/com/sparta/spring_projectclone/controller/EmailController.java
@@ -0,0 +1,36 @@
+package com.sparta.spring_projectclone.controller;
+
+import com.sparta.spring_projectclone.service.EmailService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class EmailController {
+
+    private final EmailService emailService;
+
+    @PostMapping("/emailConfirm")
+    @ApiOperation(value = "회원 가입시 이메인 인증", notes = "기존사용하고 있는 이메일을 통해 인증")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "인증 실패"),
+            @ApiResponse(code = 404, message = "사용자 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<String> emailConfirm(
+            @RequestBody @ApiParam(value="이메일정보 정보", required = true) String email) throws Exception {
+
+        String confirm = emailService.sendSimpleMessage(email);
+
+        return ResponseEntity.status(200).body(confirm);
+    }
+
+}
---
+++ spring_clone_coding/spring_projectClone/src/main/java/com/sparta/spring_projectclone/service/EmailService.java
@@ -0,0 +1,5 @@
+package com.sparta.spring_projectclone.service;
+
+public interface EmailService {
+    String sendSimpleMessage(String to) throws Exception;
+}
